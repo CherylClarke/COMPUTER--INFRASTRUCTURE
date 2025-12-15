9f058d49ade3e79b0076eb1f381e8f070440df1e
CherylClarke <cherylclark90@live.co.uk>
2025-12-15 18:13:38 +0000

    stock.py

--- module work/stocks.py
+++ module work/stocks.py
@@ -6,9 +6,14 @@ import datetime as dt
 # yahoo finance data
 import yfinance as yf
 
-# get the data 
+# get the data (downloding the data)
 df = yf.download(['AAPL', 'MSFT', 'GOOG'], period='5d', interval='1h')
 
-# current date and time 
-
+# current date and time (getting current time stamp)
 now = dt.datetime.now()
+
+# file name (creating a new filename based on new timestamp)
+filename = "../data/" + dt.datetime.now().strftime("%Y%m%d %H%M%S") + ".csv"
+
+#save data as csv (saving the new filename)
+df.to_csv(filename)
